828ae9f8190e79b2d0a6494e9ff3d8edf4ebc19b
Alain Lemaire <jaxdid@gmail.com>
2018-06-01 14:58:56 +0100

    Fetches and saves user data in Dashboard state

--- carousel/src/app/components/Dashboard/Dashboard.jsx
+++ carousel/src/app/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getJSON } from '../../utils/fetch';
 
 import './dashboard.scss';
 
-export default () => (
-  <div >
-    Hello World!!
-  </div>
-);
+export default class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userData: this.getUserData()
+    };
+  }
+
+  getUserData() {
+    return getJSON('https://s3.amazonaws.com/cdn.clearscore.com/native/interview_test/creditReportInfo.json')
+      .then(resp => this.setState({userData: resp.creditReportInfo}));
+  }
+
+  render() {
+    if (!this.state.userData) {
+      return null;
+    }
+
+    return (
+      <div>
+        {this.state.userData.score}
+      </div>
+    );
+  }
+}
